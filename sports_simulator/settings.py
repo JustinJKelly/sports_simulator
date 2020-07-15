1d21feb49f70dff302b412cb92e90539d9332dc9
Justin Kelly <justinjkelly2@gmail.com>
2020-07-14 20:44:32 -0700

    .......

--- sports_simulator/settings.py
+++ sports_simulator/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.sportsim.live','.arcane-shore-48095.herokuapp.com','0.0.0.0','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.sportsim.live','.herokuapp.com','127.0.0.1', 'localhost']
 
 
 # Application definition
